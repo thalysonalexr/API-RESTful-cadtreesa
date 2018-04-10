984882c2dffdf700aa7951ab867f5efd46001ebd
Unknown <thalysonps3@gmail.com>
2018-04-10 03:29:06 -0400

    setting method OPTIONS for CORS

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,15 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, Access-Control-Request-Headers');
 header('Accept: application/json');
+
+// The request was accepted but waits for a new request method: POST, PUT, DELETE or GET
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	header ('HTTP/1.1 202 Accepted');
+	die();
+}
 
 require realpath(__DIR__) . '/vendor/autoload.php';
 
